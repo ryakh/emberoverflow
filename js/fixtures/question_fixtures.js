ee33808b535a2215795d799b90fd5242ec9f2b7d
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 13:49:45 +0100

    Chapter 7: Updated question fixtures with author relation

--- js/fixtures/question_fixtures.js
+++ js/fixtures/question_fixtures.js
@@ -2,7 +2,7 @@ App.Question.FIXTURES = [
   {
     id:        101,
     title:    'How do I feed hamsters?',
-    author:   'Tom Dale',
+    author:    201,
     date:     '2013-01-01T12:00:00',
     question: 'Tomster cant eat using knife and a fork because his hands are \
                too small. We are looking for a way to feed him. Any ideas?'
@@ -10,7 +10,7 @@ App.Question.FIXTURES = [
   {
     id:        102,
     title:    'Are humans insane?',
-    author:   'Tomster the Hamster',
+    author:    202,
     date:     '2013-02-02T12:00:00',
     question: 'I mean are totaly nuts? Is there any hope left for them? Should \
                we hamsters try to save them?'
