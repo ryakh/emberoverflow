e6acfda0b0b549152fc4c015d84f31f57fb80218
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 15:46:10 +0100

    Chapter 10: Question controller with answerQuestion action

--- js/controllers/question_controllers.js
+++ js/controllers/question_controllers.js
@@ -32,3 +32,31 @@ App.AskQuestionController = Ember.ArrayController.extend({
     }
   }
 });
+
+App.QuestionController = Ember.ObjectController.extend({
+  needs: ['application'],
+
+  actions: {
+    answerQuestion: function() {
+      var answer = this.store.createRecord('answer', {
+        answer: this.get('answer'),
+        question: this.get('model'),
+        date: new Date()
+      });
+
+      this.get('controllers.application.signedInUser').then(function(user) {
+        answer.set('user', user);
+      });
+
+      var controller = this;
+
+      answer.save().then(function(answer) {
+        controller.get('model.answers').addObject(answer);
+
+        controller.setProperties({
+          answer: ''
+        });
+      });
+    }
+  }
+});
