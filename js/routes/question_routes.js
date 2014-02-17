52600ecf8dfbb314042ec67ce5508e9ee68017f5
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 16:39:33 +0100

    Chapter 12: Added questions route

--- js/routes/question_routes.js
+++ js/routes/question_routes.js
@@ -3,3 +3,9 @@ App.AskQuestionRoute = Ember.Route.extend({
     return this.store.findAll('question');
   }
 });
+
+App.QuestionsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('question');
+  }
+});
