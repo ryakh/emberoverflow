1220d5ba94b796d0f9d5bec6ea5038cea5e1e917
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 13:43:30 +0100

    Chapter 7: User fixtures

---
+++ js/fixtures/user_fixtures.js
@@ -0,0 +1,12 @@
+App.User.FIXTURES = [
+  {
+    id:        201,
+    fullname: 'Tom Dale',
+    email:    'tom@dale.com'
+  },
+  {
+    id:        202,
+    fullname: 'Tomster the Hamster',
+    email:    'tomster@hamster.com'
+  }
+];
