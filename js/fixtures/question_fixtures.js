5f4dbf8789e2f3fe40084fb4c29cd678625cd8df
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 13:26:45 +0100

    Chapter 6: Question fixtures

---
+++ js/fixtures/question_fixtures.js
@@ -0,0 +1,18 @@
+App.Question.FIXTURES = [
+  {
+    id:        101,
+    title:    'How do I feed hamsters?',
+    author:   'Tom Dale',
+    date:     '2013-01-01T12:00:00',
+    question: 'Tomster cant eat using knife and a fork because his hands are \
+               too small. We are looking for a way to feed him. Any ideas?'
+  },
+  {
+    id:        102,
+    title:    'Are humans insane?',
+    author:   'Tomster the Hamster',
+    date:     '2013-02-02T12:00:00',
+    question: 'I mean are totaly nuts? Is there any hope left for them? Should \
+               we hamsters try to save them?'
+  }
+];
