0cc2d4a8f3a53aa72c5c649f1d60bc4bb364f736
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 16:05:42 +0100

    Chapter 11: pluralForm property to ensure correct inflection

--- js/components/question_components.js
+++ js/components/question_components.js
@@ -1,3 +1,8 @@
 App.QuestionPreviewComponent = Ember.Component.extend({
-  answersCount: Ember.computed.alias('question.answers.length')
+  answersCount: Ember.computed.alias('question.answers.length'),
+
+  pluralForm: function() {
+    var answers = this.get('answersCount');
+    return answers === 1 ? 'answer' : 'answers'
+  }.property('answersCount')
 });
