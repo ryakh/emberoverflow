fa63dce76427d1f33acfbe4e942bb9c8a29dae6b
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 14:17:18 +0100

    Chapter 8: Binding signedInUser and userSignedIn to currentUser property

--- js/controllers/application_controllers.js
+++ js/controllers/application_controllers.js
@@ -9,11 +9,11 @@ App.IndexController = Ember.ArrayController.extend({
 App.ApplicationController = Ember.Controller.extend({
   signedInUser: function() {
     return this.store.find('user', localStorage['currentUser']);
-  }.property(),
+  }.property('App.currentUser'),
 
   userSignedIn: function() {
     return localStorage['currentUser'] != null;
-  }.property(),
+  }.property('App.currentUser'),
 
   actions: {
     signOut: function() {
