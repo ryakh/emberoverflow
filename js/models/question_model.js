59e93bdc76dfc9aae6c68e15244c904799ecc5ba
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 13:48:01 +0100

    Chapter 7: Updated author property to be a relation

--- js/models/question_model.js
+++ js/models/question_model.js
@@ -2,5 +2,5 @@ App.Question = DS.Model.extend({
   title:    DS.attr('string'),
   question: DS.attr('string'),
   date:     DS.attr('date'),
-  author:   DS.attr('string')
+  author:   DS.belongsTo('user', { async: true })
 });
