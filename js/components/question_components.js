fceb8894bd0b32a3057519e8b33f72f03ee63862
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 16:07:38 +0100

    Chapter 11: hasAnswers propery added to component

--- js/components/question_components.js
+++ js/components/question_components.js
@@ -4,5 +4,9 @@ App.QuestionPreviewComponent = Ember.Component.extend({
   pluralForm: function() {
     var answers = this.get('answersCount');
     return answers === 1 ? 'answer' : 'answers'
+  }.property('answersCount'),
+
+  hasAnswers: function() {
+    return this.get('answersCount') > 0;
   }.property('answersCount')
 });
