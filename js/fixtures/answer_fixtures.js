ed58b2e4c764b011bec891c3f5d8ac587b3d6668
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 16:26:13 +0100

    Chapter 11: Added new answer to answer fixtures

--- js/fixtures/answer_fixtures.js
+++ js/fixtures/answer_fixtures.js
@@ -4,5 +4,11 @@ App.Answer.FIXTURES = [
     answer: 'You can feed them from a bowl!',
     date:   '2013-01-01T12:00:00',
     author:  203
+  },
+  {
+    id:      302,
+    answer: 'What about a steak in a bowl?',
+    date:   '2013-02-02T12:00:00',
+    author:  202
   }
 ];
