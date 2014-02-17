0b717c997bc8204d42a4997683cf091a6dc22c4b
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 14:03:19 +0100

    Chapter 8: signedInUser and userSignedIn properties

--- js/controllers/application_controllers.js
+++ js/controllers/application_controllers.js
@@ -5,3 +5,13 @@ App.IndexController = Ember.ArrayController.extend({
     return(new Date);
   }.property()
 });
+
+App.ApplicationController = Ember.Controller.extend({
+  signedInUser: function() {
+    return this.store.find('user', localStorage['currentUser']);
+  }.property(),
+
+  userSignedIn: function() {
+    return localStorage['currentUser'] != null;
+  }.property()
+});
