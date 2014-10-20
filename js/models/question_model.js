4d11c84b50dea5acfb22b1b6560690de1d51dfd9
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-20 11:26:29 +0200

    Chapter 10: Added relation to answer into the question model

--- js/models/question_model.js
+++ js/models/question_model.js
@@ -2,5 +2,6 @@ App.Question = DS.Model.extend({
   title:    DS.attr('string'),
   question: DS.attr('string'),
   date:     DS.attr('date'),
-  author:   DS.belongsTo('user', { async: true })
+  author:   DS.belongsTo('user', { async: true }),
+  answers:  DS.hasMany('answer', { async: true })
 });
